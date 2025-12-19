e7d97002ae756b6e3144e776d58a9c1b30a340be
Amxter <amerxt2006@gmail.com>
2025-12-19 12:03:19 +0300

    Add test code

--- BusinessEM/Class1.cs
+++ BusinessEM/Class1.cs
@@ -8,5 +8,17 @@ namespace BusinessES
 {
     public class Class1
     {
+
+
+        int x; int y;
+        public Class1(int a, int b)
+        {
+            x = a; y = b;
+        }
+        public int Add()
+        {
+            return x + y;
+
+        }
     }
 }
